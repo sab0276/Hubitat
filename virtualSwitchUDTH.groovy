dd98551c5d9abc32b9d56c2fecea9a497f29cf19
Scott Barton <Scott_Barton@hotmail.com>
2021-06-11 11:01:12 -0400

    Update virtualSwitchUDTH.groovy

--- virtualSwitchUDTH.groovy
+++ virtualSwitchUDTH.groovy
@@ -11,6 +11,7 @@ metadata {
         capability "Presence Sensor"	//"present", "not present"
         capability "Smoke Detector"    //"detected", "clear", "tested"
         capability "Water Sensor"      //"dry", "wet"
+	capability "ShockSensor"	//"clear", "detected"	
 	}   
     
     preferences {
@@ -19,6 +20,7 @@ metadata {
         input name: "presence", type: "bool", title: "Presence Sensor", defaultValue: false
         input name: "smoke", type: "bool", title: "Smoke Detector", defaultValue: false
         input name: "water", type: "bool", title: "Water Detector", defaultValue: false
+	input name: "shock", type: "bool", title: "Shock Sensor", defaultValue: false
         input name: "autoOff", type: "enum", description: "", title: "Enable auto off", options: [[0:"Disabled"],[1:"1s"],[2:"2s"],[3:"3s"],[4:"4s"],[5:"5s"],[6:"6s"],[7:"7s"],[8:"8s"],[9:"9s"]], defaultValue: 0
     }
 }
@@ -30,6 +32,7 @@ def off() {
     if (presence) sendEvent(name: "presence", value: "not present")
     if (smoke) sendEvent(name: "smoke", value: "clear")
     if (water) sendEvent(name: "water", value: "dry")
+    if (shock) sendEvent(name: "shock", value: "clear")
 }
 
 def on() {
@@ -39,7 +42,8 @@ def on() {
     if (presence) sendEvent(name: "presence", value: "present")
     if (smoke) sendEvent(name: "smoke", value: "detected")
     if (water) sendEvent(name: "water", value: "wet")
-    
+    if (shock) sendEvent(name: "shock", value: "detected")
+	
     if (autoOff.toInteger()>0){
         runIn(autoOff.toInteger(), off)
     }
